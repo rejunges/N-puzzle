d7b298693177bddca04c703b4d62f799c1f2690a
Renata Junges <rzjunges@inf.ufpel.edu.br>
2019-03-25 11:10:19 -0300

    Implementação do A* com as duas heuristicas

--- jogo.py
+++ jogo.py
@@ -1,5 +1,6 @@
 import argparse
 import numpy as np
+import operator
 from random import randint 
 from node import Node
 
@@ -50,7 +51,7 @@ class Game:
 		return m
 
 	def shuffle(self, m):
-		for i in range(0,5):
+		for i in range(0,25):
 			func = randint(0,3)
 			if(func==0):
 				m = self.move_up(m)
@@ -77,7 +78,7 @@ class Game:
 
 	#Busca em profundidade com recursão
 	"""
-	def DFS(self, node, nivel):
+	def DFS(self, node, level):
 		
 		if self.verify_node(node.value):
 			return node
@@ -88,21 +89,21 @@ class Game:
 		node_left = self.move_left(node.value.copy())
 		
 		if (not np.array_equal(node_up, node.value)):
-			node.insert_up(node_up, node, node.nivel+1)
-			return self.DFS(node.up, node.nivel+1)
+			node.insert_up(node_up, node, node.level+1)
+			return self.DFS(node.up, node.level+1)
 		if (not np.array_equal(node_down, node.value)):
-			node.insert_down(node_down, node, node.nivel+1)
-			return self.DFS(node.down, node.nivel+1)
+			node.insert_down(node_down, node, node.level+1)
+			return self.DFS(node.down, node.level+1)
 		if (not np.array_equal(node_right, node.value)):
-			node.insert_right(node_right, node, node.nivel+1)
-			return self.DFS(node.right, node.nivel+1)
+			node.insert_right(node_right, node, node.level+1)
+			return self.DFS(node.right, node.level+1)
 		if (not np.array_equal(node_left, node.value)):
-			node.insert_left(node_left, node, node.nivel+1)
-			return self.DFS(node.left, node.nivel+1)
+			node.insert_left(node_left, node, node.level+1)
+			return self.DFS(node.left, node.level+1)
 	"""	
 
 	#Busca em profundidade
-	def DFS(self, node, nivel):
+	def DFS(self, node, level):
 		
 		to_visit = [node]
 
@@ -118,20 +119,20 @@ class Game:
 			node_left = self.move_left(node.value.copy())
 			
 			if (not np.array_equal(node_up, node.value)):
-				node.insert_up(node_up, node, node.nivel+1)
+				node.insert_up(node_up, node, node.level+1)
 				to_visit.insert(0, node.up)
 			if (not np.array_equal(node_down, node.value)):
-				node.insert_down(node_down, node, node.nivel+1)
+				node.insert_down(node_down, node, node.level+1)
 				to_visit.insert(0, node.down)
 			if (not np.array_equal(node_right, node.value)):
-				node.insert_right(node_right, node, node.nivel+1)
+				node.insert_right(node_right, node, node.level+1)
 				to_visit.insert(0, node.right)
 			if (not np.array_equal(node_left, node.value)):
-				node.insert_left(node_left, node, node.nivel+1)
+				node.insert_left(node_left, node, node.level+1)
 				to_visit.insert(0, node.left)
 
 	#Busca em aprofundamento iterativo
-	def IDDS(self, node_root, nivel):
+	def IDDS(self, node_root, level):
 		
 		to_visit = [node_root]
 
@@ -141,30 +142,30 @@ class Game:
 			if self.verify_node(node.value):
 				return node
 
-			if node.nivel < nivel: 
+			if node.level < level: 
 				node_up = self.move_up(node.value.copy())
 				node_down = self.move_down(node.value.copy())
 				node_right = self.move_right(node.value.copy())
 				node_left = self.move_left(node.value.copy())
 				
 				if (not np.array_equal(node_up, node.value)):
-					node.insert_up(node_up, node, node.nivel+1)
+					node.insert_up(node_up, node, node.level+1)
 					to_visit.insert(0, node.up)
 				if (not np.array_equal(node_down, node.value)):
-					node.insert_down(node_down, node, node.nivel+1)
+					node.insert_down(node_down, node, node.level+1)
 					to_visit.insert(0, node.down)
 				if (not np.array_equal(node_right, node.value)):
-					node.insert_right(node_right, node, node.nivel+1)
+					node.insert_right(node_right, node, node.level+1)
 					to_visit.insert(0, node.right)
 				if (not np.array_equal(node_left, node.value)):
-					node.insert_left(node_left, node, node.nivel+1)
+					node.insert_left(node_left, node, node.level+1)
 					to_visit.insert(0, node.left)
 	
-		return self.IDDS(node_root, nivel + 1)
+		return self.IDDS(node_root, level + 1)
 
 
 	#Busca em amplitude
-	def BFS(self, node, nivel):
+	def BFS(self, node, level):
 
 		to_visit = [node]
 		while to_visit:
@@ -179,16 +180,16 @@ class Game:
 			node_left = self.move_left(node.value.copy())
 			
 			if (not np.array_equal(node_up, node.value)):
-				node.insert_up(node_up, node, node.nivel+1)
+				node.insert_up(node_up, node, node.level+1)
 				to_visit.append(node.up)
 			if (not np.array_equal(node_down, node.value)):
-				node.insert_down(node_down, node, node.nivel+1)
+				node.insert_down(node_down, node, node.level+1)
 				to_visit.append(node.down)
 			if (not np.array_equal(node_right, node.value)):
-				node.insert_right(node_right, node, node.nivel+1)
+				node.insert_right(node_right, node, node.level+1)
 				to_visit.append(node.right)
 			if (not np.array_equal(node_left, node.value)):
-				node.insert_left(node_left, node, node.nivel+1)
+				node.insert_left(node_left, node, node.level+1)
 				to_visit.append(node.left)
 			
 	def out_of_place_heuristic(self, m):
@@ -207,7 +208,55 @@ class Game:
 					distance += abs(i-i_final) + abs(j-j_final)
 			
 		return int(distance)
+	
+	def calculate_heuristic(self, node, heuristic):
+		#Heuristica 1 para peças fora do lugar e heurística 2 para distancia de manhattan
+		if heuristic==1:
+			h = self.out_of_place_heuristic(node.value)
+		elif heuristic==2:
+			h = self.manhattan_distance_heuristic(node.value)
+		
+		return h
 
+	def A_star(self, node, level, heuristic=1):
+		#Heuristica 1 para peças fora do lugar e heurística 2 para distancia de manhattan
+		#g = node.level
+		#h = funcao heuristica
+		#f = g+h
+		open_nodes = [[node, node.level + self.calculate_heuristic(node, heuristic)]]
+	
+		while open_nodes:
+			open_nodes = sorted(open_nodes, key=operator.itemgetter(1)) #order by f_score
+			node, f_score = open_nodes.pop(0) 
+			
+			if self.verify_node(node.value):
+				return node
+			
+			#Esse if verifica se os nodos já estão abertos
+			if node.up == None and node.down == None and node.right == None and node.left == None: 
+				node_up = self.move_up(node.value.copy())
+				node_down = self.move_down(node.value.copy())
+				node_right = self.move_right(node.value.copy())
+				node_left = self.move_left(node.value.copy())
+				
+				if (not np.array_equal(node_up, node.value)):
+					node.insert_up(node_up, node, node.level+1)
+					h = self.calculate_heuristic(node.up, heuristic)
+					open_nodes.append([node.up, node.level+1 + h])
+				if (not np.array_equal(node_down, node.value)):
+					node.insert_down(node_down, node, node.level+1)
+					h = self.calculate_heuristic(node.down, heuristic)
+					open_nodes.append([node.down, node.level+1 + h])
+				if (not np.array_equal(node_right, node.value)):
+					node.insert_right(node_right, node, node.level+1)
+					h = self.calculate_heuristic(node.right, heuristic)
+					open_nodes.append([node.right, node.level+1 + h])
+				if (not np.array_equal(node_left, node.value)):
+					node.insert_left(node_left, node, node.level+1)
+					h = self.calculate_heuristic(node.left, heuristic)
+					open_nodes.append([node.left, node.level+1 + h])
+				
+			
 
 if __name__ == '__main__':
 	#Le a dimensao (nxn) do jogo
@@ -226,13 +275,26 @@ if __name__ == '__main__':
 	print(game.manhattan_distance_heuristic(root.value))
 	print("BFS")
 	final = game.BFS(root,0)
-	print(final.nivel)
+	print(final.level)
 	game.read_solution(final)
-	print("DFS")
+	#print("DFS")
 	game.bord = board
 	root = Node(game.board)
 	print("DFI")
 	final_DFI = game.IDDS(root, 0)
-	print(final_DFI.nivel)
+	print(final_DFI.level)
 	game.read_solution(final_DFI)
 	
+
+	print("A* - pecas fora do lugar")
+	game.bord = board
+	root = Node(game.board)
+	final = game.A_star(root, 1)
+	print(final.level)
+	game.read_solution(final)
+	print("A* - distancia")
+	game.bord = board
+	root = Node(game.board)
+	final = game.A_star(root, 2)
+	print(final.level)
+	game.read_solution(final)
--- node.py
+++ node.py
@@ -2,24 +2,24 @@ import numpy as np
 
 class Node:
 
-    def __init__(self, value, father = None, nivel = 0):
+    def __init__(self, value, father = None, level = 0):
         self.left = None
         self.right = None
         self.down = None
         self.up = None
         self.father = father
         self.value = value
-        self.nivel = nivel
+        self.level = level
     
-    def insert_left(self, value, father, nivel):
-        self.left = Node(value, father, nivel)
+    def insert_left(self, value, father, level):
+        self.left = Node(value, father, level)
     
-    def insert_right(self, value, father, nivel):
-        self.right = Node(value, father, nivel)
+    def insert_right(self, value, father, level):
+        self.right = Node(value, father, level)
 
-    def insert_down(self, value, father, nivel):
-        self.down = Node(value, father, nivel)
+    def insert_down(self, value, father, level):
+        self.down = Node(value, father, level)
     
-    def insert_up(self, value, father, nivel):
-        self.up = Node(value, father, nivel)
+    def insert_up(self, value, father, level):
+        self.up = Node(value, father, level)
     
